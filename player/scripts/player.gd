66bbcb37aeec0ce4aebde00a24712b8ec4b4cc5a
martynpie <martynpie@gmail.com>
2025-10-08 19:05:47 +0100

    Chapter 01 Ep 01 - Basic Player Scene

---
+++ player/scripts/player.gd
@@ -0,0 +1,16 @@
+class_name Player extends CharacterBody2D
+
+func _process( delta : float ) -> void:
+	
+	pass
+
+
+func _physics_process( _delta : float ) -> void:
+	velocity.x = 0
+	if Input.is_action_pressed( "left" ):
+		velocity.x = -100
+	elif Input.is_action_pressed( "right" ):
+		velocity.x = 100
+	velocity.y = velocity.y + 981 * _delta
+	move_and_slide()
+	pass
