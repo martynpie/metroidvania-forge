753406d777619d70972a5225bc114ca16347cd9f
martynpie <martynpie@gmail.com>
2025-10-12 13:44:57 +0100

    Chapter 01 Ep 02 - Building Player State Machine
    Run and Idle, plus the guts of the FSM

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,16 +1,84 @@
 class_name Player extends CharacterBody2D
 
-func _process( delta : float ) -> void:
-	
+#region /// State Machine Variables
+var states : Array[ PlayerState ]
+var current_state : PlayerState : 
+	get : return states.front() #dot front returns the first element of the array
+var previous_state : PlayerState :
+	get : return states[ 1 ] #returns the state in array position 2 (obvs posn 1 is index zero)
+#endregion
+
+#region /// standard variables
+var direction : Vector2 = Vector2.ZERO
+var gravity : float = 981
+#endregion
+
+
+func _ready() -> void:
+	#initialise states
+	initialise_states()
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	change_state( current_state.handle_input( event ) )
+
+
+func _process( _delta : float ) -> void:
+	update_direction()
+	change_state( current_state.process( _delta ) )
 	pass
 
 
 func _physics_process( _delta : float ) -> void:
-	velocity.x = 0
-	if Input.is_action_pressed( "left" ):
-		velocity.x = -100
-	elif Input.is_action_pressed( "right" ):
-		velocity.x = 100
-	velocity.y = velocity.y + 981 * _delta
+	velocity.y += gravity * _delta
 	move_and_slide()
+	change_state( current_state.physics_process( _delta ) )
+	pass
+
+
+func initialise_states() -> void:
+	states = []
+	#gather all the states
+	for c in $States.get_children():
+		if c is PlayerState:
+			states.append( c )
+			c.player = self #refers to the class name of this script, i.e. Player
+		pass
+	
+	if states.size() == 0:
+		print("no states detected")
+		return #this would break the function early if there were no states - otherwise setting the first state would bug the game out
+	
+	
+	#initialise all the states
+	for state in states:
+		state.init() #calls the init function in the state script
+	
+	#set our first state
+	change_state( current_state )
+	current_state.enter()
+	pass
+
+
+func change_state( new_state : PlayerState ) -> void:
+	if new_state == null:
+		return
+	elif new_state == current_state:
+		return
+	
+	if current_state: #short for "if current state is a valid state", so we can proceed to exit and change to next state
+		current_state.exit()
+	
+	states.push_front( new_state ) #push front is a sausage machine function for arrays, putting the new state to the front and shunting everything else to suit
+	current_state.enter()
+	states.resize( 3 ) #keeps the size of the states array at 3, so we're not just adding thousands of states
+	pass
+
+
+func update_direction() -> void:
+	#var prev_direction : Vector2 = direction
+	direction = Input.get_vector( "left" , "right" , "up" , "down" )
+	
+	#do more stuff later
 	pass
---
+++ player/states/00_player_state.gd
@@ -0,0 +1,36 @@
+#This script defines what a state needs to perform
+@icon( "res://player/states/state.svg" )
+class_name PlayerState extends Node
+
+var player : Player #a variable that HAS to be of Class 'Player' - this is strict player 
+var next_state : PlayerState
+
+#region /// state references
+# reference to all other states
+#endregion 
+
+#What happens when this state is initialised?
+func init() -> void:
+	pass
+
+#What happens when we enter this state?
+func enter() -> void:
+	pass
+
+#What happens when we exit this state?
+func exit() -> void:
+	pass
+
+#What happens when an input is pressed?
+func handle_input( _event : InputEvent ) -> PlayerState:
+
+	return next_state
+
+#What happens each process tick in this state?
+func process( _delta: float ) -> PlayerState:
+	return next_state
+
+#What happens each physics_process tick in this state?
+func physics_process( _delta: float ) -> PlayerState:
+	
+	return next_state
